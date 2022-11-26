9d7c793d3f68679aa1d2e6d884f0a7cf8c6de2df
William Roque <william.aroque@gmail.com>
2022-11-26 12:49:12 -0700

    Started working on eigenvalue extensions for Calc.

--- calc.el
+++ calc.el
@@ -1,7 +1,22 @@
+(setq math-additional-units '((SM "1.989e30 * kg" "Solar masses")))
 
-;;; Custom units stored by Calc on Wed Nov 16 11:00:56 2022
-(setq math-additional-units '(
-  (SM "1.989e30 * kg" "Solar masses")
-))
-;;; End of custom units
+(defmath eigval (A)
+  (interactive 1 "eigval")
+  (and (square-matrixp A)
+       (solve-for
+        (det (- A (calcFunc-diag '(var λ) (1- (length A))))) 0
+        '(var λ) 'all)))
 
+(defmath eigvec (A)
+  (interactive 1 "eigvec")
+  (and (square-matrixp A)
+       (let ((I (calcFunc-diag 1 (1- (length A))))
+             (vecs (calcFunc-vec)))
+         (foreach ((λ (cdr (eigval A)))
+                   (x '(var x)))
+                  (setq vecs
+                        (cons (solve-for
+                               (* x (- A (* λ I))) 0
+                               x 'all)
+                              vecs)))
+         vecs)))
